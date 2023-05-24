e4cf08dbbdb9dc9fb75d9f63dc96b7a34106bbe8
Bill Ola Rasmussen <billra@gmail.com>
2023-05-24 20:42:29 +0200

    report file count

--- scan.py
+++ scan.py
@@ -2,16 +2,19 @@
 # Bill Ola Rasmussen
 
 from argparse import ArgumentParser
+from collections import defaultdict
 import os
 
 def doFile(file):
     print(f'do: {file}')
 
-def doAllFiles(root):
+def doAllFiles(root,ftc):
     for path, dirs, files in os.walk(root):
         # Skip hidden directories
         dirs[:]=[d for d in dirs if not d.startswith('.')] # note slice assignment
         for file in files:
+            ext=os.path.splitext(file)[1]
+            ftc[ext if ext else 'none']+=1
             doFile(os.path.join(path, file))
 
 def main():
@@ -21,7 +24,11 @@ def main():
     args=parser.parse_args()
     root=os.path.abspath(args.root)
     print(f'parsing site at: {root}')
-    doAllFiles(root)
+    ftc=defaultdict(int) # file type count
+    doAllFiles(root,ftc)
+    print('files:')
+    for key,val in sorted(ftc.items()):
+        print(f'  {key}: {val}')
     print('done.')
 
 if __name__ == '__main__':
