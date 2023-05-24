2845fe9ec34ce9be60b76775c9a7a3ca2e42168a
Bill Ola Rasmussen <billra@gmail.com>
2023-05-24 12:18:35 +0200

    down to individual files

--- scan.py
+++ scan.py
@@ -1,14 +1,27 @@
+# scan.py
+# Bill Ola Rasmussen
+
 from argparse import ArgumentParser
 import os
+
+def doFile(file):
+    print(f'do: {file}')
+
+def doAllFiles(root):
+    for path, dirs, files in os.walk(root):
+        # Skip hidden directories
+        dirs[:]=[d for d in dirs if not d.startswith('.')] # note slice assignment
+        for file in files:
+            doFile(os.path.join(path, file))
 
 def main():
     print('scan')
     parser = ArgumentParser()
-    parser.add_argument('-d', '--directory', default='.', help='directory to scan')
+    parser.add_argument('-r', '--root', default='.', help='root of site to scan, defaults to cwd')
     args=parser.parse_args()
-    path=os.path.abspath(args.directory)
-    print(f'parsing site at: {path}')
-
+    root=os.path.abspath(args.root)
+    print(f'parsing site at: {root}')
+    doAllFiles(root)
     print('done.')
 
 if __name__ == '__main__':
